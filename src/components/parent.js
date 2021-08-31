f2930597156ad30f9f0392d3dd38f88a8834a121
Keith Dahlby <dahlbyk@gmail.com>
2021-08-31 11:51:18 -0500

    Add/use a <Child /> component

--- src/components/parent.js
+++ src/components/parent.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Child from './child';
 
 class Parent extends React.Component {
   render() {
-    return 'hi!';
+    return (
+      <div>
+        <p>My name is Keith</p>
+        <ol>
+          <li><Child name="TBD" /></li>
+          <li><Child name="FDD" /></li>
+        </ol>
+      </div>
+    )
   }
 }
 
